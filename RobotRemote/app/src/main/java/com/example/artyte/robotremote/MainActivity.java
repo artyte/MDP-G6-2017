52261a3217b05302ed22560ec1a102ef695999f8
artyte <artyte@gmail.com>
2017-02-04 19:54:05 +0800

    Working refresh button

--- RobotRemote/app/src/main/java/com/example/artyte/robotremote/MainActivity.java
+++ RobotRemote/app/src/main/java/com/example/artyte/robotremote/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -22,7 +23,26 @@ public class MainActivity extends Activity {
     private ListView btListView;
     private ArrayList<String> mDeviceList = new ArrayList<String>();
     private BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();;
-    private IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+    private IntentFilter filter = new IntentFilter();
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (action.equals(BluetoothDevice.ACTION_FOUND)) {
+                BluetoothDevice device = intent
+                        .getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                mDeviceList.add(device.getName() + "\n" + device.getAddress());
+                btListView.setAdapter(new ArrayAdapter<String>(context,
+                        android.R.layout.simple_list_item_1, mDeviceList));
+            }
+            else if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                if (intent.getIntExtra(BluetoothAdapter.EXTRA_STATE,
+                        BluetoothAdapter.ERROR) == BluetoothAdapter.STATE_ON) {
+                    mBluetoothAdapter.startDiscovery();
+                }
+            }
+        }
+    };
 
 
 
@@ -31,14 +51,11 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (!mBluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, 1);
-        } //start bluetooth
-
-        //initialise bluetooth listview so that other modules can use it
         btListView = (ListView) findViewById(R.id.btListView);
+
         mBluetoothAdapter.startDiscovery();
+        filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
         registerReceiver(mReceiver, filter);
 
         btListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -48,7 +65,6 @@ public class MainActivity extends Activity {
                 startActivity(intent);
             }
         });
-
     }
 
     @Override
@@ -65,22 +81,22 @@ public class MainActivity extends Activity {
         super.onDestroy();
     }
 
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                BluetoothDevice device = intent
-                        .getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                mDeviceList.add(device.getName() + "\n" + device.getAddress());
-                btListView.setAdapter(new ArrayAdapter<String>(context,
-                        android.R.layout.simple_list_item_1, mDeviceList));
-            }
-        }
-    };
-
     public void refreshPage(MenuItem item) {
-        ((ArrayAdapter)btListView.getAdapter()).clear();
-        mBluetoothAdapter.cancelDiscovery();
-        mBluetoothAdapter.startDiscovery();
+        if(btListView.getAdapter() != null) {
+            ((ArrayAdapter) btListView.getAdapter()).clear();
+            ((ArrayAdapter) btListView.getAdapter()).notifyDataSetChanged();
+            mBluetoothAdapter.cancelDiscovery();
+            mBluetoothAdapter.startDiscovery();
+        }
+    }
+
+    public void onOffBT(MenuItem item) {
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
+        }
+        else {
+            mBluetoothAdapter.disable();
+        }
     }
 }
